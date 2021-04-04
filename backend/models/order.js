2db25256f4846b0214d6ffa2d8ea21ee33d68f61
Cesar Carrasco <carrascomcesar@gmail.com>
2021-04-04 12:12:04 -0400

    Order Model Created

---
+++ backend/models/order.js
@@ -0,0 +1,101 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  shippingInfo: {
+    address: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    zipCode: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  orderItems: [
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+      image: {
+        type: String,
+        required: true,
+      },
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "Product",
+      },
+    },
+  ],
+  paymentInfo: {
+    id: {
+      type: String,
+    },
+    status: {
+      type: String,
+    },
+    itemsPrice: {
+      type: String,
+      required: true,
+      default: 0.0,
+    },
+    taxPrice: {
+      type: String,
+      required: true,
+      default: 0.0,
+    },
+    shippingPrice: {
+      type: String,
+      required: true,
+      default: 0.0,
+    },
+    totalPrice: {
+      type: String,
+      required: true,
+      default: 0.0,
+    },
+  },
+  orderStatus: {
+    type: String,
+    required: true,
+    default: "Processing",
+  },
+  deliveredAt: {
+    type: Date,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  paidAt: {
+    type: Date,
+  },
+});
+
+module.exports = mongoose.model("Order", orderSchema);
