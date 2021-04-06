cbb80b3000371828e6c4f9f456cea1dbd707d090
Cesar Carrasco <carrascomcesar@gmail.com>
2021-04-06 11:47:43 -0400

    Header & Footer Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
+import Footer from "./components/layout/Footer";
+import Header from "./components/layout/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Shop It</p>
+      <header>
+        <Header></Header>
+        <Footer></Footer>
       </header>
     </div>
   );
---
+++ frontend/src/components/layout/Footer.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+
+export default function Footer() {
+  return (
+    <Fragment>
+      <footer className="py-1">
+        <p className="text-center mt-1">
+          Shopping Cart - 2021-2022, All Rights Reserved
+        </p>
+      </footer>
+    </Fragment>
+  );
+}
