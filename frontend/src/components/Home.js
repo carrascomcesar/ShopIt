8508178f75261e736d9352726db2f65872597b3b
Cesar Carrasco <carrascomcesar@gmail.com>
2021-04-06 17:29:37 -0400

    Implement Custom Titles with Helmet

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,9 +1,12 @@
 import React, { Fragment } from "react";
+import MetaData from "./layout/MetaData";
+
 import Product from "./Product";
 
 export default function Home() {
   return (
     <Fragment>
+      <MetaData title={"Buy the Best Products Online"}></MetaData>
       <h1 id="products_heading">Latest Products</h1>
 
       <section id="products" className="container mt-5">
