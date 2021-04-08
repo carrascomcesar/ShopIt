5934f9d66cc537fafc07285e8aa8f6973d55e8e6
Cesar Carrasco <carrascomcesar@gmail.com>
2021-04-08 17:02:36 -0400

    Product Details COmponent Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
 import Home from "./components/Home";
+import ProductDetails from "./components/product/ProductDetails";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Header></Header>
         <div className="container container-fluid">
           <Route path="/" component={Home} exact />
+          <Route path="/product/:id" component={ProductDetails} exact />
         </div>
         <Footer></Footer>
       </div>
