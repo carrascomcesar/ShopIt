6c667b1c38852947b2df3ce4e1e490169659ab65
Cesar Carrasco <carrascomcesar@gmail.com>
2021-04-08 10:48:02 -0400

    Product Details Reducer & Actions

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -5,6 +5,9 @@ import {
   ALL_PRODUCTS_SUCCESS,
   ALL_PRODUCTS_FAIL,
   CLEAR_ERRORS,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
 } from "../constants/productConstants";
 
 export const getProducts = () => async (dispatch) => {
@@ -24,6 +27,29 @@ export const getProducts = () => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: ALL_PRODUCTS_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
+
+export const getProductDetails = (id) => async (dispatch) => {
+  try {
+    // Set Loading to True and Products as an Empty Array
+    dispatch({
+      type: PRODUCT_DETAILS_REQUEST,
+    });
+
+    // Get Data from API Backend
+    const { data } = await axios.get(`/api/v1/product/${id}`);
+
+    // Dispatch Success
+    dispatch({
+      type: PRODUCT_DETAILS_SUCCESS,
+      payload: data.product,
+    });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
       payload: error.response.data.message,
     });
   }
