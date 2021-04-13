ad8690a491e9471a4fd4a45f43829f3b0ac94a3e
Cesar Carrasco <carrascomcesar@gmail.com>
2021-04-13 13:30:44 -0400

    Filter by price NOT WORKING

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -70,7 +70,7 @@ export default function Home({ match }) {
                           placement: "top",
                           visible: true,
                         }}
-                        value="price"
+                        value={price}
                         onChange={(price) => setPrice(price)}
                       />
                     </div>
