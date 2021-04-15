0888894f4a916c4160e481d82be76af59f5b2b58
Cesar Carrasco <carrascomcesar@gmail.com>
2021-04-15 10:01:13 -0400

    Added Register Reducers and Actions

--- frontend/src/actions/authActions.js
+++ frontend/src/actions/authActions.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 import {
-  LOGIN_REQUEST,
-  LOGIN_SUCCESS,
-  LOGIN_FAIL,
+  LOGIN_USER_REQUEST,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_FAIL,
+  REGISTER_USER_REQUEST,
+  REGISTER_USER_SUCCESS,
+  REGISTER_USER_FAIL,
   CLEAR_ERRORS,
 } from "../constants/authConstants";
 
@@ -10,7 +13,7 @@ import {
 export const login = (email, password) => async (dispatch) => {
   try {
     dispatch({
-      type: LOGIN_REQUEST,
+      type: LOGIN_USER_REQUEST,
     });
     const config = {
       headers: {
@@ -24,12 +27,43 @@ export const login = (email, password) => async (dispatch) => {
       config
     );
     dispatch({
-      type: LOGIN_SUCCESS,
+      type: LOGIN_USER_SUCCESS,
       payload: data.user,
     });
   } catch (error) {
     dispatch({
-      type: LOGIN_FAIL,
+      type: LOGIN_USER_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
+
+// Register
+export const Register = (userData) => async (dispatch) => {
+  try {
+    dispatch({
+      type: REGISTER_USER_REQUEST,
+    });
+
+    // Data configuration
+    const config = {
+      headers: {
+        "Content-type": "multipart/form-data",
+      },
+    };
+
+    // Data sent to backend with axios
+    const { data } = await axios.post("api/v1/register", { userData }, config);
+
+    // If success, send payload to backend
+    dispatch({
+      type: REGISTER_USER_SUCCESS,
+      payload: data.user,
+    });
+  } catch (error) {
+    // If error send error message.
+    dispatch({
+      type: REGISTER_USER_FAIL,
       payload: error.response.data.message,
     });
   }
--- frontend/src/reducers/authReducers.js
+++ frontend/src/reducers/authReducers.js
@@ -1,19 +1,24 @@
 import {
-  LOGIN_REQUEST,
-  LOGIN_SUCCESS,
-  LOGIN_FAIL,
+  LOGIN_USER_REQUEST,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_FAIL,
+  REGISTER_USER_REQUEST,
+  REGISTER_USER_SUCCESS,
+  REGISTER_USER_FAIL,
   CLEAR_ERRORS,
 } from "../constants/authConstants";
 
 export const authReducer = (state = { user: {} }, action) => {
   switch (action.type) {
-    case LOGIN_REQUEST:
+    case LOGIN_USER_REQUEST:
+    case REGISTER_USER_REQUEST:
       return {
         loading: true,
         isAuthenticated: false,
       };
 
-    case LOGIN_SUCCESS:
+    case REGISTER_USER_SUCCESS:
+    case LOGIN_USER_SUCCESS:
       return {
         ...state,
         loading: false,
@@ -21,7 +26,8 @@ export const authReducer = (state = { user: {} }, action) => {
         user: action.payload,
       };
 
-    case LOGIN_FAIL:
+    case LOGIN_USER_FAIL:
+    case REGISTER_USER_FAIL:
       return {
         ...state,
         loading: false,
